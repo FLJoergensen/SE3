0de26b990d35580a805161bb0adc8b412e397b78
FLJoergensen (4joergen) <fljoergensen1994@googlemail.com>
2015-11-09 03:27:02 +0100

    3
    1.2 verkuerzt

--- se3_3.rkt
+++ se3_3.rkt
@@ -48,25 +48,13 @@
   (let ((r (assoc v dict)))
     (cond ((not (equal? r #f)) (cdr r)))))
 
-(define (Text->p text tabelle)
-  (t->p (string->list text) tabelle '()))
-(define (t->p liste tab ergebnis)
-  (cond ((empty? liste) ergebnis)
-        (else (cons (tabellenSurch (car liste) tabelle) ergebnis))))
-(define (tabellenSurchAll surch tabelle ergebnisListe)
-  (cond ((empty? tabelle) ergebnisListe)
-        ((equal? surch (caar tabelle)) (tabellenSurch surch (cdr tabelle) (cons (cdar tabelle) ergebnisListe)))
-        (else (tabellenSurch surch (cdr tabelle) ergebnisListe))))
-(define (tabellenSurch surch tabelle)
-  (cond ((empty? tabelle) null)
-        ((equal? surch (caar tabelle)) (cons (cdar tabelle) '()))
-        (else (tabellenSurch surch (cdr tabelle)))))
-
 ;1.3
+;;(UP #\a)
 (define (UP chr)
   (char-upcase chr))
 
 ;1.4
+;;(Buchstabieren-einesTextes "TEXTOHNELEHRZEICHEN")
 (define (Buchstabieren-eines-Textes text)
   (BeT (string->list (string-upcase text)) tabelle '()))
 (define (transE v dict)
