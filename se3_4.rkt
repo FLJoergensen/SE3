e2db91e305144b108d081abbc5422d22e7a3d3cc
Sonius94 <tomkastek@aol.com>
2015-11-12 12:31:48 +0100

    Aufgabe 1

--- se3_4.rkt
+++ se3_4.rkt
@@ -1,1 +1,12 @@
 #lang racket
+
+1.
+(max(min 2 (- 2 5)) 0) ; 0
+'(+ (- 2 13) 11) ; '(+ (- 2 13) 11)
+(cadr '(Alle Jahre wieder)) ; 'Jahre
+(cddr '(kommt (das Weihnachtfest))) ; '()
+(cdadr '(kommt (das . Weihnachtfest))) ; 'Weihnachtfest
+(cons 'Listen '(ganz einfach und)) ; '(Listen ganz einfach und)
+(cons 'Paare 'auch) ; '(Paare . auch)
+(equal? (list 'Racket 'Prolog 'Java) '(Racket Prolog Java)) ; #t
+(eq? (list 'Racket 'Prolog 'Java) (cons 'Racket '(Prolog Java))) ; #f
