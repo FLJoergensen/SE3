4b53c737a7d29796b72dda2ecc7583718fe0780c
FLJoergensen (4joergen) <fljoergensen1994@googlemail.com>
2015-11-18 13:21:45 +0100

    5
    +test

--- se3_5.rkt
+++ se3_5.rkt
@@ -1,1 +1,7 @@
 #lang racket
+
+(require se3-bib/butterfly-module)
+
+(show_butterfly ’red ’stripes ’curved ’rhomb)
+(show_butterfly ’yellow ’stripes ’straight ’hexagon)
+(show_butterfly ’blue ’dots ’curly ’ellipse)
