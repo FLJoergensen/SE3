dee36fec72162f821e0c139d4d60afc4f340bf4e
Spokey <4buczko@informatik.uni-hamburg.de>
2015-11-20 14:46:36 +0100

    gaaanz viel blabla

--- se3_5.rkt
+++ se3_5.rkt
@@ -14,6 +14,19 @@
 (define fluegelform '(rhombisch hexagonal elliptisch))
 (define fuehlerform '(gerade gekruemmt geschweift))
 
+;Einem Schmetterling muss von jedem dieser Listen ein Element zugeordnet sein.
+;Zusätzlich besitzt jeder Schmetterling ein zweites rezessives Element von jedem Merkmal,
+;welches nicht sichtbar ist, aber bei der Vererbung eine Rolle spielt.
+;Die dominanten Merkmale des Elternpaares sind fest vorgegeben. Die rezessiven
+;müssen mittels einer Zufall-Funktion zugeteilt werden.
+;Dann brauchen wir eine Funktion, die uns zufällig Kind-Schmetterlinge aus
+;den Merkmalen der Eltern erzeugt. Hierbei ist zu beachten, dass für jedes Merkmal
+;zufällig das dominante oder das rezessive Merkmal des jeweiligen Elternteils weitervererbt
+;werden kann. Das dominantere von Beiden bestimmt schlussendlich das Aussehen des
+;Kind-Schmetterlings.
+;Am Ende brauchen wir eine Funktion, die uns den Schmetterling ausgibt, sodass
+;klar ist, welches seine dominanten Merkmale sind.
+
 (define (gibrezesList x xs)
   (cond ((empty? xs) '())
         ((equal? x (car xs)) xs)
