1c1647b14be7eeb0c66a6cfc28f2552b8b67b5d0
Spokey <4buczko@informatik.uni-hamburg.de>
2015-11-20 14:31:24 +0100

    the beginning....

--- se3_5.rkt
+++ se3_5.rkt
@@ -5,3 +5,11 @@
 (show-butterfly 'red 'stripes 'curved 'rhomb)
 (show-butterfly 'yellow 'stripes 'straight 'hexagon)
 (show-butterfly 'blue 'dots 'curly 'ellipse)
+
+;Wir speichern alle Merkmale, die ein Schmetterling haben kann jeweils in einer Liste.
+;Die Dominanz ist durch die Reihenfolge der Elemente in der Liste geordnet.
+;In der Liste wird mit der dominantesten Art eines Merkmals begonnen.
+(define musterung '(punkte streifen sterne))
+(define fluegelfarbe '(rot gruen blau gelb))
+(define fluegelform '(rhombisch hexagonal elliptisch))
+(define fuehlerform '(gerade gekruemmt geschweift))
