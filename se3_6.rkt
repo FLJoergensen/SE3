2fccb82087f5a2c3cc4349c4ae14a06a48b67167
FLJoergensen (4joergen) <fljoergensen1994@googlemail.com>
2015-11-26 13:31:46 +0100

    6
    2.1 kp

--- se3_6.rkt
+++ se3_6.rkt
@@ -9,7 +9,22 @@ Merge-sort: Ist geschachtelt, da in der merge Rekursion auf merge-sort zurückge
             Außerdem ist es Baumrekursiv und indirekt, da versch. Rekursive Funktionen aufgerufen werden.
 |#
 
+(require racket/trace)
+
+(define (sorted? xs comp)
+  (cond ((empty? (cdr xs)) #t)
+        ((comp (car xs) (cadr xs)) (sorted? (cdr xs) comp))
+        (else #f)))
+(define (IS xs comp c)
+  (cond ((sorted? xs comp) xs)
+        (else (IS (Insertion-Sort xs comp) comp (- c 1)))))
 (define (Insertion-Sort xs comp)
-  (cond ((empty? xs) (display xs) '())
-        ((comp (car xs) (cadr xs)) (display xs) (cons (car xs) (Insertion-Sort (cdr xs) comp)))
+  (cond ((empty? (cdr xs))  xs)
+        ((comp (car xs) (cadr xs)) (cons (car xs) (Insertion-Sort (cdr xs) comp)))
         (else (cons (cadr xs) (Insertion-Sort (cons (car xs) (cddr xs)) comp)))))
+
+;(trace Insertion-Sort)
+(trace IS)
+(IS '(2 3 5 1 6 9 8 7 4) < 9)
+(IS '(9 3 5 4 6 2 8 7 1) < 9)
+(IS '(9 8 7 6 5 4 3 2 1) < 9)
