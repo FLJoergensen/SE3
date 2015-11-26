3c86ba0927812f3d92153ea1a72aa78f56feea40
Spokey <4buczko@informatik.uni-hamburg.de>
2015-11-26 12:32:33 +0100

    öy öy das ist schon fertig

--- se3_6.rkt
+++ se3_6.rkt
@@ -1,1 +1,10 @@
 #lang racket
+#Aufgabe 1:
+Kopfstück: ist linaer weil mit der Funktion nur eine Rekursion aufgeruefen wird und direkt, 
+           da in Kopfstück wieder Kopfstück aufgerufen wird
+Endstück: Genauso wie in Kopfstück, ruft sich die Funktion nur selbst auf
+Merge: Ist eine Baumrekursion, da im else-Zweig sich merge zweimal selbst aufruft. 
+       Es ist direkt da merge nur merge rekursiv aufruft.
+Merge-sort: Ist geschachtelt, da in der merge Rekursion auf merge-sort zurückgegriffen wird. 
+            Außerdem ist es Baumrekursiv und indirekt, da versch. Rekursive Funktionen aufgerufen werden.
+|#
