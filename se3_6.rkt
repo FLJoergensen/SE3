a88379cd9da608ee92693148c225a9d6b13ae3d6
FLJoergensen (4joergen) <fljoergensen1994@googlemail.com>
2015-11-26 13:07:02 +0100

    6
    2.1 todo

--- se3_6.rkt
+++ se3_6.rkt
@@ -1,5 +1,5 @@
 #lang racket
-#Aufgabe 1:
+#|Aufgabe 1:
 Kopfstück: ist linaer weil mit der Funktion nur eine Rekursion aufgeruefen wird und direkt, 
            da in Kopfstück wieder Kopfstück aufgerufen wird
 Endstück: Genauso wie in Kopfstück, ruft sich die Funktion nur selbst auf
@@ -8,3 +8,8 @@ Merge: Ist eine Baumrekursion, da im else-Zweig sich merge zweimal selbst aufruf
 Merge-sort: Ist geschachtelt, da in der merge Rekursion auf merge-sort zurückgegriffen wird. 
             Außerdem ist es Baumrekursiv und indirekt, da versch. Rekursive Funktionen aufgerufen werden.
 |#
+
+(define (Insertion-Sort xs comp)
+  (cond ((empty? xs) (display xs) '())
+        ((comp (car xs) (cadr xs)) (display xs) (cons (car xs) (Insertion-Sort (cdr xs) comp)))
+        (else (cons (cadr xs) (Insertion-Sort (cons (car xs) (cddr xs)) comp)))))
